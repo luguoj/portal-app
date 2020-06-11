91844a8f8ae62918793155754efca0d5bea7b890
zhoudd <328704089@qq.com>
2020-06-11 16:40:11 +0800

    proxy.psr-ajax默认启用简单排序模式

--- src/data/proxy/Ajax.js
+++ src/data/proxy/Ajax.js
@@ -2,6 +2,7 @@ Ext.define('PSR.data.proxy.Ajax', {
     extend: 'Ext.data.proxy.Ajax',
     alias: 'proxy.psr-ajax',
     limitParam: 'pageSize',
+    simpleSortMode: true,
     reader: {
         type: 'json',
         totalProperty: 'totalCount',
