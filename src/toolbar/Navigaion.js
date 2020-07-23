13ad204f9e0d140d95cd3d383209ec07fd775957
zhoudd <328704089@qq.com>
2020-07-23 09:44:02 +0800

    导航工具条改为标题工具条类型

--- src/toolbar/Navigaion.js
+++ src/toolbar/Navigaion.js
@@ -1,15 +1,17 @@
 Ext.define('PSR.toolbar.Navigation', {
-    extend: 'Ext.Toolbar',
+    extend: 'Ext.TitleBar',
     xtype: 'psr-toolbar-navigation',
     config: {
         goBackHandler: null
     },
+    docked: 'top',
     constructor: function (config) {
         var me = this;
         me.callParent([config]);
         if (me.getGoBackHandler()) {
             me.add({
                 tooltip: '返回', iconCls: 'x-fa fa-arrow-left',
+                align: 'left',
                 handler: me.getGoBackHandler()
             });
         }
