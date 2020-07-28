a53e035f4f2ef8f98b6024d3a07fccddd5d52fec
zhoudd <328704089@qq.com>
2020-07-28 10:40:06 +0800

    左侧停靠垂直布局表单

---
+++ src/panel/form/Left.js
@@ -0,0 +1,6 @@
+Ext.define('PSR.panel.form.Left', {
+    extend: 'Ext.form.Panel',
+    xtype: 'psr-panel-form-left',
+    docked: 'left', width: '20%', minWidth: '100', resizable: {split: true, edges: 'east'}, scrollable: 'y',
+    hidden: true
+});
