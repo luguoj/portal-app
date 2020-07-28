b17d34d293c3c71ae9c83a6d92cda2220b80e276
zhoudd <328704089@qq.com>
2020-07-28 10:42:27 +0800

    顶部工具栏容器

---
+++ src/toolbar/TopContainer.js
@@ -0,0 +1,8 @@
+Ext.define('PSR.toolbar.TopContainer', {
+    extend: 'Ext.Toolbar',
+    xtype: 'psr-toolbar-topcontainer',
+    docked: 'top', padding: 0, layout: {type: 'hbox', wrap: true},
+    defaults: {
+        xtype: 'toolbar'
+    }
+});
