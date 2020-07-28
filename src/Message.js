80225a65609d0775c28a1c7c6666d59cea42ba73
zhoudd <328704089@qq.com>
2020-07-28 10:45:01 +0800

    消息增加错误信息接口

--- src/Message.js
+++ src/Message.js
@@ -2,5 +2,8 @@ Ext.define('PSR.Message', {
     singleton: true,
     info: function (message) {
         Ext.toast(message);
+    },
+    error: function (message) {
+        Ext.MessageBox.alert('错误', message);
     }
 });
