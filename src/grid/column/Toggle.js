079df0ace2c22d9fb87b54857c764836f6c9549b
zhoudd <328704089@qq.com>
2020-08-03 11:01:53 +0800

    增加组件grid-column-toggle,增加样式p-confirm-important,p-decline-important

---
+++ src/grid/column/Toggle.js
@@ -0,0 +1,32 @@
+Ext.define('PSR.grid.column.Toggle', {
+    extend: 'Ext.grid.column.Column',
+    xtype: 'psr-grid-column-toggle',
+    width: 51, maxWidth: 51, minWidth: 51, align: 'center',
+    menuDisabled: true,
+    config: {
+        flagIndex: '',
+        toggleHandler: 'toggle',
+        disabledBinding: ''
+    },
+    constructor: function (config) {
+        const flagIndex = config.flagIndex,
+            disabledBinding = config.disabledBinding,
+            toggleHandler = config.toggleHandler;
+        config.cell = {
+            xtype: 'widgetcell',
+            widget: {
+                xtype: 'button',
+                width: 45,
+                bind: {
+                    tooltip: '{record.' + flagIndex + '?"是":"否"}',
+                    iconCls: '{record.' + flagIndex + '?"x-fa fa-check p-confirm-important":"x-fa fa-times p-decline-important"}',
+                    hidden: '{record.isPath}',
+                    disabled: '{' + disabledBinding + '}',
+                    ui: '{(' + disabledBinding + '?"alt ":"") + (record.' + flagIndex + '?"confirm":"decline")}'
+                },
+                handler: toggleHandler
+            }
+        };
+        this.callParent([config]);
+    }
+});
