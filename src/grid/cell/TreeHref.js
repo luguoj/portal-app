d38fa01e0f0f752111b30c132c024a3c28204589
zhoudd <328704089@qq.com>
2020-08-07 09:36:30 +0800

    bugfix 修订路径树枝干记录删除时超链接没有取消

--- src/grid/cell/TreeHref.js
+++ src/grid/cell/TreeHref.js
@@ -7,23 +7,39 @@ Ext.define('PSR.grid.cell.TreeHref', {
     isRecord: function (record) {
         return record && record.data && !record.data.isPath;
     },
-    updateRawValue: function (rawValue) {
-        var dom = this.bodyElement.dom,
+    actionDom: null,
+    applyRawValue: function (rawValue) {
+        const dom = this.bodyElement.dom,
             value = rawValue == null ? '' : rawValue,
             me = this,
             record = this.getRecord(),
             handler = this.getHandler();
+        let actionDom = this.actionDom;
         if (this.isRecord(record) && value && value.trim()) {
-            if (!dom.firstChild || dom.firstChild.tagName != 'A') {
+            if (!actionDom) {
                 dom.innerHTML = '<a href="javascript:void(0)"></a>'
+                actionDom = this.actionDom = dom.firstChild;
+                if (handler) {
+                    actionDom.onclick = function () {
+                        Ext.callback(handler, me.getScope(), [me.getRecord()], 0, me);
+                    };
+                }
+                this.updateRawValue(value);
             }
-            dom = dom.firstChild;
-            if (handler) {
-                dom.onclick = function () {
-                    Ext.callback(handler, me.getScope(), [me.getRecord()], 0, me);
-                };
+        } else {
+            if (actionDom) {
+                dom.innerHTML = '';
+                this.actionDom = null;
+                this.updateRawValue(value);
             }
         }
+        return value;
+    },
+    updateRawValue: function (rawValue) {
+        const actionDom = this.actionDom,
+            dom = actionDom ? actionDom : this.bodyElement.dom,
+            value = rawValue == null ? '' : rawValue;
+
         if (this.getEncodeHtml()) {
             dom.textContent = value;
         } else {
