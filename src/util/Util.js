90458f6dadbf3a69ef393fc81997cd8167f0e01e
zhoudd <328704089@qq.com>
2020-08-25 16:38:34 +0800

    增加方法PSR.Util.saveAs下载数据保存为本地文件

--- src/util/Util.js
+++ src/util/Util.js
@@ -6,5 +6,18 @@ Ext.define('PSR.Util', {
         context.font = font;
         const metrics = context.measureText(text);
         return metrics.width;
+    },
+    saveAsElement: document.createElementNS("http://www.w3.org/1999/xhtml", "a"),
+    saveAs: function (name, data, type) {
+        const element = this.saveAsElement,
+            ev = document.createEvent("MouseEvents"),
+            urlObject = window.URL || window.webkitURL || window,
+            export_blob = new Blob([data], {type: type ? type : ''});
+        element.href = urlObject.createObjectURL(export_blob);
+        element.download = name;
+        ev.initMouseEvent("click", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+        element.dispatchEvent(ev);
+        urlObject.revokeObjectURL(element.href);
+        element.href = '';
     }
 });
