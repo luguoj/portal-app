1016f6685ac82930920ca1ac58de9aeb2abca4c8
zhoudd <328704089@qq.com>
2020-08-26 10:45:52 +0800

    废弃curdpanel

--- src/panel/crud/List.js
+++
@@ -1,8 +0,0 @@
-Ext.define('PSR.panel.crud.List', {
-    extend: 'PSR.panel.crud.Base',
-    xtype: 'psr-panel-crud-list',
-    searchView: {
-        xtype: 'psr-panel-crud-search-list'
-    },
-    detailsView: {}
-});
--- src/panel/crud/Tree.js
+++
@@ -1,8 +0,0 @@
-Ext.define('PSR.panel.crud.Tree', {
-    extend: 'PSR.panel.crud.Base',
-    xtype: 'psr-panel-crud-tree',
-    searchView: {
-        xtype: 'psr-panel-crud-search-tree'
-    },
-    detailsView: {}
-});
