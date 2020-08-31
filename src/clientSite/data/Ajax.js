4f58de97aae771c418bc2721a0a9e8c067fa520e
zhoudd <328704089@qq.com>
2020-08-31 11:39:21 +0800

    增加访问拒绝提示信息，认证信息无效增加重试机制

--- src/clientSite/data/Ajax.js
+++ src/clientSite/data/Ajax.js
@@ -16,7 +16,18 @@ Ext.define('PSR.clientSite.data.Ajax', {
     hCallFailure: function (response, opt) {
         if (response) {
             if (response.status == '401') {
-                PSR.Message.error("认证信息无效");
+                if (!opt.retryTimes || opt.retryTimes < 5) {
+                    opt.retryTimes = opt.retryTimes ? opt.retryTimes + 1 : 1;
+                    setTimeout(function () {
+                        PSR.clientSite.data.Ajax.request(opt);
+                    }, 500);
+                    return;
+                } else {
+                    delete opt.retryTimes;
+                    PSR.Message.error("授权信息无效");
+                }
+            } else if (response.status == '403') {
+                PSR.Message.error("不允许访问")
             } else {
                 PSR.Message.error(response.statusText);
             }
