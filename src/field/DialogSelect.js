686eef3df2a11255039b08842c58f9428b50561f
zhoudd <328704089@qq.com>
2021-03-16 08:18:28 +0800

    fix bug 对话框弹出异常

--- src/field/DialogSelect.js
+++ src/field/DialogSelect.js
@@ -7,9 +7,6 @@ Ext.define('PSR.field.dialogSelect', {
     config: {
         required: false,
         isTree: false,
-        picker: {
-            xtype: 'psr-panel-dataviewpicker'
-        },
         placeholder: '',
         displayField: 'displaytext',
         valueField: 'value',
@@ -40,17 +37,85 @@ Ext.define('PSR.field.dialogSelect', {
         this.trigger.setDisabled(value);
     },
     expand: function () {
-        this.pickerDialog.show();
+        const me = this,
+            placeholder = me.getPlaceholder(),
+            isTree = me.getIsTree(),
+            displayField = me.getDisplayField(),
+            valueField = me.getValueField(),
+            filterFields = me.getFilterFields(),
+            extraParams = me.getExtraParams(),
+            paramConverter = me.getParamConverter(),
+            store = me.getStore(),
+            pickerDialog = Ext.create({
+                xtype: 'dialog',
+                padding: 0,
+                width: '50%',
+                height: '50%',
+                layout: 'fit',
+                items: [{
+                    xtype: 'toolbar',
+                    docked: 'top',
+                    items: [{
+                        tooltip: '过滤', iconCls: 'x-fa fa-filter',
+                        enableToggle: true,
+                        toggleHandler: function (btn, pressed) {
+                            picker.expandFilter(pressed);
+                        }
+                    }, {
+                        tooltip: '搜索', iconCls: 'x-fa fa-search',
+                        handler: function () {
+                            picker.refresh();
+                        }
+                    }, '->', {
+                        tooltip: '确认', iconCls: 'x-fa fa-check', reference: 'btnOK',
+                        ui: 'confirm',
+                        handler: function (btn) {
+                            me.setValue(btn.selection.get(valueField));
+                            delete this.picker;
+                            pickerDialog.close();
+                        },
+                        disabled: true,
+                    }, {
+                        tooltip: '取消', iconCls: 'x-fa fa-times',
+                        ui: 'decline',
+                        handler: function () {
+                            delete this.picker;
+                            pickerDialog.close();
+                        }
+                    }]
+                }, {
+                    xtype: 'psr-panel-dataviewpicker',
+                    isTree: isTree,
+                    fieldTitle: placeholder,
+                    displayField: displayField,
+                    filterFields: filterFields,
+                    extraParams: extraParams,
+                    paramConverter: paramConverter,
+                    store: store,
+                    listeners: {
+                        selectionchange: function (picker, selections) {
+                            const btnOK = picker.up('dialog').down('button[reference="btnOK"]');
+                            if (selections && selections.length > 0 && selections[0].get('isRecord')) {
+                                btnOK.setDisabled(false);
+                                btnOK.selection = selections[0];
+                            } else {
+                                btnOK.setDisabled(true);
+                            }
+                        }
+                    }
+                }],
+                listeners: {
+                    show: function () {
+                        picker.refresh();
+                    }
+                }
+            }),
+            picker = this.picker = pickerDialog.down('psr-panel-dataviewpicker');
+        pickerDialog.show();
     },
     constructor: function (config) {
         const me = this,
-            isTree = config.isTree || this.config.isTree,
             placeholder = config.placeholder || this.config.placeholder,
-            displayField = config.displayField || this.config.displayField,
-            valueField = config.valueField || this.config.valueField,
-            filterFields = [].concat(config.filterFields || this.config.filterFields || []),
-            extraParams = config.extraParams || this.config.extraParams,
-            paramConverter = config.paramConverter || this.config.paramConverter,
             required = config.required || this.config.required,
             valueFieldName = config.valueFieldName || this.config.valueFieldName,
             value = config.value || this.config.value,
@@ -83,81 +148,10 @@ Ext.define('PSR.field.dialogSelect', {
                 }
             };
         config.items = [valueHolder, displayer, trigger];
-        me.pickerDialog = Ext.create({
-            xtype: 'dialog',
-            padding: 0,
-            width: '50%',
-            height: '50%',
-            layout: 'fit',
-            items: [{
-                xtype: 'toolbar',
-                docked: 'top',
-                items: [{
-                    tooltip: '过滤', iconCls: 'x-fa fa-filter',
-                    enableToggle: true,
-                    toggleHandler: function (btn, pressed) {
-                        me.picker.expandFilter(pressed);
-                    }
-                }, {
-                    tooltip: '搜索', iconCls: 'x-fa fa-search',
-                    handler: function () {
-                        me.picker.refresh();
-                    }
-                }, '->', {
-                    tooltip: '确认', iconCls: 'x-fa fa-check', reference: 'btnOK',
-                    ui: 'confirm',
-                    handler: function (btn) {
-                        me.setValue(btn.selection.get(valueField));
-                        me.pickerDialog.hide();
-                    },
-                    disabled: true,
-                }, {
-                    tooltip: '取消', iconCls: 'x-fa fa-times',
-                    ui: 'decline',
-                    handler: function () {
-                        me.pickerDialog.hide();
-                    }
-                }]
-            }, {
-                xtype: 'psr-panel-dataviewpicker',
-                isTree: isTree,
-                fieldTitle: placeholder,
-                displayField: displayField,
-                filterFields: filterFields,
-                extraParams: extraParams,
-                paramConverter: paramConverter,
-                listeners: {
-                    selectionchange: function (picker, selections) {
-                        const btnOK = picker.up('dialog').down('button[reference="btnOK"]');
-                        if (selections && selections.length > 0 && selections[0].get('isRecord')) {
-                            btnOK.setDisabled(false);
-                            btnOK.selection = selections[0];
-                        } else {
-                            btnOK.setDisabled(true);
-                        }
-                    }
-                }
-            }],
-            listeners: {
-                show: function () {
-                    me.picker.refresh();
-                }
-            }
-        });
-        me.picker = me.pickerDialog.down('psr-panel-dataviewpicker');
         me.callParent([config]);
         me.valueHolder = me.down('textfield[reference="fValueHolder"]');
         me.displayer = me.down('textfield[reference="fDisplayer"]');
         me.trigger = me.down('button');
-        me.picker.on({
-            confirm: function (record) {
-                me.setValue(record[0].data[me.getValueField()]);
-                me.pickerDialog.hide();
-            },
-            cancel: function () {
-                me.pickerDialog.hide();
-            }
-        });
     },
     updateStore: function (store) {
         if (this.picker) {
