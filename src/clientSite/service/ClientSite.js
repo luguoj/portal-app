bc7a4d028ca3c548968e3bb04016868be383d3ea
zhoudd <328704089@qq.com>
2021-03-19 13:33:02 +0800

    optimize 优化模块源码加载逻辑

--- src/clientSite/service/ClientSite.js
+++ src/clientSite/service/ClientSite.js
@@ -12,37 +12,16 @@ Ext.define('PSR.clientSite.service.ClientSite', {
         });
     },
     loadModuleSrc: function (opt) {
-        Ext.Ajax.request({
-            url: window.moduleSite + '/module/' + opt.moduleId + '/index.js',
-            method: 'GET',
-            disableCaching: true,
-            success: function (response) {
-                const responseText = response.responseText;
-                try {
-                    (new Function(responseText))();
-                    if (opt.success) {
-                        opt.success(response);
-                    }
-                } catch (err) {
-                    PSR.Message.error(err.message);
-                    console.error(err);
-                    if (opt.failure) {
-                        opt.failure(response, err);
-                    }
-                }
-                if (opt.complete) {
-                    opt.complete();
-                }
-            },
-            failure: function (response) {
-                if (opt.failure) {
-                    opt.failure(response);
-                }
-                if (opt.complete) {
-                    opt.complete();
-                }
-            }
-        });
+        const moduleSrcUrl = window.moduleSite + '/module/' + opt.moduleId + '/index.js';
+        const scriptEl = document.createElement('script');
+        scriptEl.src = moduleSrcUrl;
+        document.body.appendChild(scriptEl);
+        if (opt.success) {
+            opt.success();
+        }
+        if (opt.complete) {
+            opt.complete();
+        }
     },
     loadModuleAction: function (opt) {
         PSR.clientSite.Ajax.request({
