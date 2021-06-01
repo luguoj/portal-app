9ab8f19d28da04fc8ba8a550de50c06a8e4a9eab
zhoudd <328704089@qq.com>
2021-06-01 15:00:03 +0800

    update 重构提取数据函数

--- src/util/Xlsx.js
+++ src/util/Xlsx.js
@@ -4,17 +4,7 @@ Ext.define('PSR.util.Xlsx', {
         if (opt && opt.file) {
             const reader = new FileReader();
             reader.onload = function (e) {
-                const data = XLSX.read(e.target.result, {
-                    type: 'binary'
-                });
-                const jsonData = {};
-                if (data.SheetNames && data.SheetNames.length > 0) {
-                    for (let i = 0; i < data.SheetNames.length; i++) {
-                        const sheetName = data.SheetNames[i],
-                            sheetJson = XLSX.utils.sheet_to_json(data.Sheets[sheetName]);
-                        jsonData[sheetName] = sheetJson;
-                    }
-                }
+                const jsonData = PSR.util.Xlsx.extractData(e.target.result);
                 if (opt.success) {
                     opt.success(jsonData);
                 }
@@ -22,10 +12,24 @@ Ext.define('PSR.util.Xlsx', {
             reader.readAsBinaryString(opt.file);
         }
     },
+    extractData: function (result) {
+        const data = XLSX.read(result, {
+            type: 'binary'
+        });
+        const jsonData = {};
+        if (data.SheetNames && data.SheetNames.length > 0) {
+            for (let i = 0; i < data.SheetNames.length; i++) {
+                const sheetName = data.SheetNames[i],
+                    sheetJson = XLSX.utils.sheet_to_json(data.Sheets[sheetName]);
+                jsonData[sheetName] = sheetJson;
+            }
+        }
+        return jsonData;
+    },
     write: function (opt) {
         if (opt) {
             if (opt.data) {
-                const workbook = {SheetNames: [], Sheets:{}};
+                const workbook = {SheetNames: [], Sheets: {}};
                 for (const sheetName in opt.data) {
                     workbook.SheetNames.push(sheetName);
                     workbook.Sheets[sheetName] = XLSX.utils.json_to_sheet(opt.data[sheetName]);
