f6817bed1f72ec2a3d31197a659233d1e64cd509
zhoudd <328704089@qq.com>
2021-06-02 09:55:23 +0800

    fix bug 某些浏览器主页多产生一个BODY标签

--- resources/desktop-index/desktop-index.js
+++ resources/desktop-index/desktop-index.js
@@ -18,33 +18,26 @@ function initDesktopEnv(opt) {
         console.log('got message from ' + event.origin + ': ' + event.data);
     }, false);
     document.addEventListener('DOMContentLoaded', function () {
+        document.getElementsByTagName('body')[0].insertAdjacentHTML('beforeend',
+            '\n' +
+            '<div id="splash">\n' +
+            '    <div class=\"splash-background\" style=\"background-image: url(' + opt.background + ');">\n' +
+            '        <div class="splash-login-pos">\n' +
+            '            <div class="splash-login-box">\n' +
+            '                <div id="splash-loading" style="padding:8px 16px;display: flex;text-align:center;">\n' +
+            '                    <div class="fa fa-spinner fa-spin" style="margin:0 6px 0 0;font-size:24px;height: 24px;width: 24px"></div>\n' +
+            '                    <div style="font: 600 18px/24px Roboto, sans-serif">&#21152;&#36733;&#20013;...</div>\n' +
+            '                </div>\n' +
+            '                <iframe allowtransparency="true" frameborder="0" id="splash-login" style="width:100%;height:100%" src="' + window.clientSite + '/"></iframe>\n' +
+            '            </div>\n' +
+            '        </div>\n' +
+            '    </div>\n' +
+            '</div>\n'
+        );
         const splash_loading = document.getElementById("splash-loading"),
-            splash_login = window.document.getElementById("splash-login"),
-            splash = document.getElementsByClassName('splash-background')[0];
-        splash.style.backgroundImage = 'url(' + opt.background + ')';
-        splash_login.src = window.clientSite + "/";
+            splash_login = window.document.getElementById("splash-login");
         splash_login.addEventListener('load', function () {
             splash_loading.style.display = 'none';
         });
     });
-    document.getElementsByTagName('html')[0].insertAdjacentHTML('beforeend',
-        '\n' +
-        '<body class="launching">\n' +
-        '<div id="splash">\n' +
-        '    <div class="splash-background">\n' +
-        '        <div class="splash-login-pos">\n' +
-        '            <div class="splash-login-box">\n' +
-        '                <div id="splash-loading" style="padding:8px 16px;display: flex;text-align:center;">\n' +
-        '                    <div class="fa fa-spinner fa-spin"\n' +
-        '                         style="margin:0 6px 0 0;font-size:24px;height: 24px;width: 24px"></div>\n' +
-        '                    <div style="font: 600 18px/24px Roboto, sans-serif">&#21152;&#36733;&#20013;...</div>\n' +
-        '                </div>\n' +
-        '                <iframe allowtransparency="true" frameborder="0" id="splash-login"\n' +
-        '                        style="width:100%;height:100%"></iframe>\n' +
-        '            </div>\n' +
-        '        </div>\n' +
-        '    </div>\n' +
-        '</div>\n' +
-        '</body>'
-    )
 }
