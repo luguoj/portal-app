0a6ee2e0e7d9fda717b8b2f18c8488a02abc22c0
zhoudd <328704089@qq.com>
2021-11-02 14:00:54 +0800

    授权数据维护直接使用标准功能

--- module/desktop/src/console/authorization/view/DataView.js
+++
@@ -1,5 +0,0 @@
-Ext.define('PortalApp.view.console.authorization.data.DataView', {
-    extend: 'PSR.view.entityCRUD.data.DataView',
-    xtype: 'console-authorization-dataview',
-    application:'authorization'
-});
