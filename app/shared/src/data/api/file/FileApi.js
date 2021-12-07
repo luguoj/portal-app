d5dee159bf34284043e06f90072c2686bec8e031
zhoudd <328704089@qq.com>
2021-12-07 16:06:29 +0800

    api接口 [patch]file/api/file

--- app/shared/src/data/api/file/FileApi.js
+++ app/shared/src/data/api/file/FileApi.js
@@ -10,5 +10,57 @@ Ext.define('PSR.data.api.file.FileApi', {
             withAuthToken: true,
             disableCaching: true
         });
+    },
+    patch: function (opt) {
+        const formData = new FormData();
+        if (opt.file) {
+            formData.append('file', opt.file);
+            opt.dlgprogress = Ext.Msg.show({
+                title: '上传中...',
+                progress: true,
+                closable: false
+            });
+        }
+        const params = {version: opt.version};
+        if (opt.storageService) {
+            params['storage_service'] = opt.storageService;
+        }
+        if (opt.versionControl) {
+            params['version_control'] = opt.versionControl;
+        }
+        if (opt.abandoned) {
+            params['abandoned'] = opt.abandoned;
+        }
+        PSR.data.Ajax.request({
+            method: 'PATCH',
+            headers: {
+                "Content-Type": null
+            },
+            url: this.getAPIUrl() + '/' + opt.id,
+            withAuthToken: true,
+            params: params,
+            rawData: formData,
+            bizSuccess: function (data) {
+                PSR.util.Message.info('上传成功');
+                if (opt.success) {
+                    opt.success(data);
+                }
+            },
+            complete: function () {
+                if (opt.dlgprogress) {
+                    opt.dlgprogress.close();
+                    delete opt.dlgprogress;
+                }
+            },
+            uploadprogress: function (e) {
+                if (opt.dlgprogress) {
+                    if (e.loaded == e.total) {
+                        opt.dlgprogress.updateProgress(e.loaded / e.total, e.loaded + ' / ' + e.total + ',保存中...')
+                    } else {
+                        opt.dlgprogress.updateProgress(e.loaded / e.total, e.loaded + ' / ' + e.total)
+                    }
+                }
+            }
+        });
     }
 });
