f6607512b9c443f4b8798123c42d528349703fd2
zhoudd <328704089@qq.com>
2021-12-08 15:49:23 +0800

    更新上传，下载，修改API

--- app/shared/src/data/api/file/FileApi.js
+++ app/shared/src/data/api/file/FileApi.js
@@ -6,21 +6,47 @@ Ext.define('PSR.data.api.file.FileApi', {
     download: function (opt) {
         const url = this.getAPIUrl();
         PSR.data.File.download({
-            url: url + '/' + opt.id,
+            url: url + '/' + opt.id + '/download',
+            withAuthToken: true
+        });
+    },
+    upload: function (opt) {
+        const formData = new FormData();
+        formData.append('file', opt.file);
+        const dlgprogress = Ext.Msg.show({
+            title: '上传中...',
+            progress: true,
+            closable: false
+        });
+        const params = {version: opt.version};
+        PSR.data.Ajax.request({
+            method: 'PUT',
+            headers: {
+                "Content-Type": null
+            },
+            url: this.getAPIUrl() + '/' + opt.id + '/upload',
             withAuthToken: true,
-            disableCaching: true
+            params: params,
+            rawData: formData,
+            bizSuccess: function (data) {
+                PSR.util.Message.info('上传成功');
+                if (opt.success) {
+                    opt.success(data);
+                }
+            },
+            complete: function () {
+                dlgprogress.close();
+            },
+            uploadprogress: function (e) {
+                if (e.loaded == e.total) {
+                    dlgprogress.updateProgress(e.loaded / e.total, e.loaded + ' / ' + e.total + ',保存中...')
+                } else {
+                    dlgprogress.updateProgress(e.loaded / e.total, e.loaded + ' / ' + e.total)
+                }
+            }
         });
     },
     patch: function (opt) {
-        const formData = new FormData();
-        if (opt.file) {
-            formData.append('file', opt.file);
-            opt.dlgprogress = Ext.Msg.show({
-                title: '上传中...',
-                progress: true,
-                closable: false
-            });
-        }
         const params = {version: opt.version};
         if (opt.storageService) {
             params['storage_service'] = opt.storageService;
@@ -39,26 +65,10 @@ Ext.define('PSR.data.api.file.FileApi', {
             url: this.getAPIUrl() + '/' + opt.id,
             withAuthToken: true,
             params: params,
-            rawData: formData,
             bizSuccess: function (data) {
-                PSR.util.Message.info('上传成功');
+                PSR.util.Message.info('修改成功');
                 if (opt.success) {
                     opt.success(data);
-                }
-            },
-            complete: function () {
-                if (opt.dlgprogress) {
-                    opt.dlgprogress.close();
-                    delete opt.dlgprogress;
-                }
-            },
-            uploadprogress: function (e) {
-                if (opt.dlgprogress) {
-                    if (e.loaded == e.total) {
-                        opt.dlgprogress.updateProgress(e.loaded / e.total, e.loaded + ' / ' + e.total + ',保存中...')
-                    } else {
-                        opt.dlgprogress.updateProgress(e.loaded / e.total, e.loaded + ' / ' + e.total)
-                    }
                 }
             }
         });
