28860782234a31827c9c43dbf55759102cc49349
zhoudd <328704089@qq.com>
2021-12-17 13:23:24 +0800

    update 分组管理模块操作视图

--- app/desktop/src/view/portal-console/group/group-module-action/GroupModuleActionView.js
+++ app/desktop/src/view/portal-console/group/group-module-action/GroupModuleActionView.js
@@ -4,9 +4,108 @@ Ext.define('PortalApp.view.portalConsole.group.GroupModuleActionView', {
     config: {
         group: null
     },
+    layout: 'fit',
+    tbar: {
+        items: [{
+            iconCls: 'x-fa fa-redo-alt',
+            handler: 'hBtnRefresh',
+            bind: {
+                disabled: '{!portalId}'
+            }
+        }]
+    },
+    items: [{
+        xtype: 'treepanel',
+        rootVisible: false,
+        columns: [{
+            text: '授权',
+            xtype: 'widgetcolumn',
+            width: 48,
+            resizable: false,
+            menuDisabled: true,
+            widget: {
+                xtype: 'button',
+                iconCls: 'x-fa fa-file',
+                handler: 'hBtnEnabled',
+                bind: {
+                    hidden: '{!record.action}',
+                    iconCls: '{record.granted?"x-fa fa-check-circle psr-color-confirm":"x-fa fa-check psr-color-disabled"}'
+                }
+            }
+        }, {
+            xtype: 'treecolumn',
+            flex: 1,
+            text: '操作',
+            dataIndex: 'text',
+            renderer: function (value, metaData, record) {
+                if (record.get('module')) {
+                    return '<b>[' + record.get('module').get('code') + ']</b> ' + record.get('module').get('description');
+                } else if (record.get('action')) {
+                    return '<b>[' + record.get('action').get('code') + ']</b> ' + record.get('action').get('description');
+                } else {
+                    return value;
+                }
+            }
+        }],
+        bind: {
+            store: '{moduleActionTree}',
+        }
+    }],
+    listeners: {
+        afterrender: 'onAfterRendered'
+    },
+    updateGroup: function (value) {
+        this.getViewModel().set('group', value);
+        if (this.rendered) {
+            this.getController().loadData();
+        }
+    },
     controller: 'portalconsole-group-groupmoduleactionviewcontroller',
     viewModel: {
-        data: {},
-        stores: {}
+        data: {
+            group: null
+        },
+        stores: {
+            moduleActionTree: {
+                fields: ['id', 'module', 'action', 'granted', 'expanded'],
+                type: 'tree',
+                root: {
+                    expanded: true
+                }
+            },
+            modules: {
+                type: 'entity',
+                application: 'portal',
+                domainType: 'org.psr.platform.portal.entity.ModuleEntity',
+                remoteFilter: true,
+                remoteSort: false,
+                autoLoad: false,
+                listeners: {
+                    load: 'onDataLoad'
+                }
+            },
+            actions: {
+                type: 'entity',
+                application: 'portal',
+                domainType: 'org.psr.platform.portal.entity.ModuleActionEntity',
+                remoteFilter: true,
+                remoteSort: false,
+                autoLoad: false,
+                listeners: {
+                    load: 'onDataLoad'
+                }
+            },
+            groupModuleActions: {
+                type: 'entity',
+                application: 'portal',
+                domainType: 'org.psr.platform.portal.entity.GroupModuleActionEntity',
+                remoteSort: true,
+                remoteFilter: true,
+                autoLoad: false,
+                listeners: {
+                    load: 'onDataLoad'
+                }
+            }
+        }
     }
 });
