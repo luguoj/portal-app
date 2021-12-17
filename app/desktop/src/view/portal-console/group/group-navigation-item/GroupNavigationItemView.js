c103c48865440ac98b197f4356211010f40c47ef
zhoudd <328704089@qq.com>
2021-12-17 09:31:57 +0800

    update 分组管理导航项目视图

--- app/desktop/src/view/portal-console/group/group-navigation-item/GroupNavigationItemView.js
+++ app/desktop/src/view/portal-console/group/group-navigation-item/GroupNavigationItemView.js
@@ -4,9 +4,97 @@ Ext.define('PortalApp.view.portalConsole.group.GroupNavigationItemView', {
     config: {
         group: null
     },
+    layout: 'fit',
+    tbar: {
+        items: ['门户:', {
+            xtype: 'combobox',
+            allowBlank: false,
+            emptyText: '选择门户',
+            editable: false,
+            valueField: 'id',
+            displayField: 'description',
+            bind: {store: '{portals}'},
+            listeners: {
+                change: 'onCombPortalChange'
+            }
+        }, {
+            iconCls: 'x-fa fa-redo-alt',
+            handler: 'hBtnRefresh',
+            bind: {
+                disabled: '{!portalId}'
+            }
+        }]
+    },
+    items: [{
+        xtype: 'treepanel',
+        columns: [{
+            text: '授权',
+            xtype: 'widgetcolumn',
+            width: 48,
+            resizable: false,
+            menuDisabled: true,
+            widget: {
+                xtype: 'button',
+                iconCls: 'x-fa fa-file',
+                handler: 'hBtnEnabled',
+                bind: {
+                    hidden: '{!record.isView}',
+                    iconCls: '{record.granted?"x-fa fa-check-circle psr-color-confirm":"x-fa fa-check psr-color-disabled"}'
+                }
+            }
+        }, {
+            xtype: 'treecolumn',
+            flex: 1,
+            text: '标题',
+            dataIndex: 'text'
+        }],
+        bind: {
+            store: '{navigationItemTree}',
+            disabled: '{!portalId}'
+        }
+    }],
+    updateGroup: function (value) {
+        this.getViewModel().set('group', value);
+    },
     controller: 'portalconsole-group-groupnavigationitemviewcontroller',
     viewModel: {
-        data: {},
-        stores: {}
+        data: {
+            group: null,
+            portalId: null
+        },
+        stores: {
+            portals: {
+                type: 'entity',
+                application: 'portal',
+                domainType: 'org.psr.platform.portal.entity.PortalEntity',
+                autoLoad: true
+            },
+            navigationItemTree: {
+                fields: ['id', 'version', 'portalId', 'parentId', 'isView', 'text', 'iconCls', 'content', 'granted'],
+                type: 'entitytree',
+                transform: 'parentTree',
+                application: 'portal',
+                domainType: 'org.psr.platform.portal.entity.NavigationItemEntity',
+                remoteSort: false,
+                remoteFilter: true,
+                autoLoad: false,
+                rootText: '导航节点',
+                sorters: [{property: 'sort', direction: 'ASC'}],
+                listeners: {
+                    load: 'onDataLoad'
+                }
+            },
+            groupNavigationItems: {
+                type: 'entity',
+                application: 'portal',
+                domainType: 'org.psr.platform.portal.entity.GroupNavigationItemEntity',
+                remoteSort: true,
+                remoteFilter: true,
+                autoLoad: false,
+                listeners: {
+                    load: 'onDataLoad'
+                }
+            }
+        }
     }
 });
--- app/desktop/src/view/portal-console/group/group-navigation-item/GroupNavigationItemViewController.js
+++ app/desktop/src/view/portal-console/group/group-navigation-item/GroupNavigationItemViewController.js
@@ -1,4 +1,72 @@
 Ext.define('PortalApp.view.portalConsole.group.GroupNavigationItemViewController', {
     extend: 'Ext.app.ViewController',
     alias: 'controller.portalconsole-group-groupnavigationitemviewcontroller',
+    onCombPortalChange: function (comb, newValue) {
+        this.getViewModel().set('portalId', newValue);
+        this.getView().down('treepanel').setDisabled(false);
+        this.getStore('navigationItemTree').addFilter(
+            {
+                property: 'portalId',
+                operator: '==',
+                value: newValue
+            }, true);
+        this.loadData();
+    },
+    hBtnRefresh: function () {
+        this.loadData();
+    },
+    loadData: function () {
+        this.getStore('groupNavigationItems').addFilter(
+            {
+                property: 'groupId',
+                operator: '==',
+                value: this.getViewModel().get('group').get('id')
+            }
+        );
+        this.getStore('navigationItemTree').load();
+        this.getStore('groupNavigationItems').load();
+    },
+    onDataLoad: function () {
+        const navigationItemTreeStore = this.getStore('navigationItemTree'),
+            groupNavigationItemStore = this.getStore('groupNavigationItems');
+        if (navigationItemTreeStore.isLoaded() && groupNavigationItemStore.isLoaded()) {
+            const grantMap = {};
+            for (let i = 0; i < groupNavigationItemStore.getData().items.length; i++) {
+                const groupNavigationItem = groupNavigationItemStore.getData().items[i];
+                grantMap[groupNavigationItem.get('navigationItemId')] = groupNavigationItem;
+            }
+            for (const byIdMapKey in navigationItemTreeStore.byIdMap) {
+                navigationItemTreeStore.byIdMap[byIdMapKey].set('granted', grantMap[byIdMapKey]);
+            }
+        }
+    },
+    hBtnEnabled: function (btn) {
+        const me = this,
+            group = this.getViewModel().get('group'),
+            record = btn.lookupViewModel().get('record');
+        if (record.get('granted')) {
+            PortalApp.data.api.entity.EntityCRUDApi.delete({
+                application: 'portal',
+                domainType: 'org.psr.platform.portal.entity.GroupNavigationItemEntity',
+                ids: [record.get('granted').id],
+                success: function (data) {
+                    PSR.util.Message.info('保存成功');
+                    me.loadData();
+                }
+            });
+        } else {
+            PortalApp.data.api.entity.EntityCRUDApi.create({
+                application: 'portal',
+                domainType: 'org.psr.platform.portal.entity.GroupNavigationItemEntity',
+                values: {
+                    groupId: group.get('id'),
+                    navigationItemId: record.get('id')
+                },
+                success: function (data) {
+                    PSR.util.Message.info('保存成功');
+                    me.loadData();
+                }
+            });
+        }
+    }
 });
