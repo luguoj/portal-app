cddd2771aeee1f7ebd9a7faddb8c5b75df17c46f
zhoudd <328704089@qq.com>
2021-12-23 11:01:02 +0800

    update Message 扩展info方法以支持弹框消息,增加warn级别消息方法

--- common/shared/src/util/Message.js
+++ common/shared/src/util/Message.js
@@ -1,11 +1,31 @@
 Ext.define('PSR.util.Message', {
     singleton: true,
-    info: function (message) {
-        Ext.toast(message);
+    info: function (message, title, callback) {
+        if (title) {
+            Ext.Msg.show({
+                iconCls: Ext.Msg.INFO,
+                title: title,
+                message: message,
+                buttons: Ext.Msg.OK,
+                fn: callback
+            });
+        } else {
+            Ext.toast(message);
+        }
     },
     error: function (message, callback) {
         console.error(new Error(message));
         Ext.Msg.alert('错误', message, callback);
+    },
+    warn: function (message) {
+        console.warn(message);
+        Ext.Msg.show({
+            iconCls: Ext.Msg.WARNING,
+            title: '警告',
+            message: message,
+            buttons: Ext.Msg.OK,
+            fn: callback
+        });
     },
     confirm: function (message, callback) {
         Ext.Msg.confirm('确认', message, function (buttonId) {
