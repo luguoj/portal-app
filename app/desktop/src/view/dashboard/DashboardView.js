42b389d53252074f63f748bd905da94c978dadc8
zhoudd <328704089@qq.com>
2022-01-06 16:53:39 +0800

    update 概览视图取消编辑功能

--- app/desktop/src/view/dashboard/DashboardView.js
+++ app/desktop/src/view/dashboard/DashboardView.js
@@ -4,53 +4,11 @@ Ext.define('PortalApp.view.dashboard.DashboardView', {
     layout: {type: 'vbox', align: 'stretch'},
     scrollable: 'y',
     tbar: {
-        items: ['仪表盘:', {
-            xtype: 'combobox',
-            bind: {
-                disabled: '{editing}'
-            }
+        items: ['模板:', {
+            xtype: 'combobox'
         }, {
             iconCls: 'x-fa fa-redo-alt',
             handler: 'hBtnRefresh'
-        }, '|', {
-            iconCls: 'x-fa fa-plus',
-            handler: 'hBtnAdd'
-        }, {
-            iconCls: 'x-fa fa-edit',
-            enableToggle: true,
-            bind: {
-                disabled: '{editing}',
-                pressed: '{editing}'
-            },
-            listeners: {
-                toggle: 'onBtnEditToggle'
-            }
-        }, {
-            iconCls: 'x-fa fa-save',
-            bind: {
-                hidden: '{!editing}'
-            },
-            handler: 'hBtnSave'
-        }, {
-            iconCls: 'x-fa fa-times',
-            bind: {
-                hidden: '{!editing}',
-                handler: 'hBtnCancel'
-            }
-        }, {
-            iconCls: 'x-fa fa-file-import',
-            tooltip: '导入配置',
-            bind: {
-                hidden: '{editing}'
-            },
-            handler: 'hBtnImport'
-        }, {
-            iconCls: 'x-fa fa-file-export',
-            tooltip: '导出配置',
-            bind: {
-                hidden: '{editing}'
-            },
-            handler: 'hBtnExport'
         }],
         bind: {
             hidden: '{fullscreen}'
@@ -64,7 +22,6 @@ Ext.define('PortalApp.view.dashboard.DashboardView', {
     controller: 'dashboard-dashboardviewcontroller',
     viewModel: {
         data: {
-            editing: false
         }
     }
 });
