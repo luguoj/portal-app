fac4f31b8b08022ee76ac132c076a06fbefb0e46
zhoudd <328704089@qq.com>
2022-01-07 11:53:41 +0800

    update 根据树形下拉选择概览模板，加载概览

--- app/desktop/src/view/dashboard/DashboardView.js
+++ app/desktop/src/view/dashboard/DashboardView.js
@@ -5,7 +5,35 @@ Ext.define('PortalApp.view.dashboard.DashboardView', {
     scrollable: 'y',
     tbar: {
         items: ['模板:', {
-            xtype: 'combobox'
+            xtype: 'treepicker',
+            flex: 1,
+            valueField: 'id',
+            displayField: 'code',
+            rootVisible: false,
+            emptyText: '选择模板',
+            columns: [{
+                xtype: 'treecolumn',
+                flex: 1,
+                dataIndex: 'code',
+                renderer: function (value, metaData, record) {
+                    if (record.get('isRecord')) {
+                        return '<u><b>' + record.get('text') + '</b></u>';
+                    } else {
+                        return record.get('text');
+                    }
+                },
+            }],
+            bind: {
+                store: '{dashboardTemplateTree}'
+            },
+            onItemClick: function (view, record, node, rowIndex, e) {
+                if (record.get('isRecord')) {
+                    this.selectItem(record);
+                }
+            },
+            listeners: {
+                select: 'onTreePickSelect'
+            }
         }, {
             iconCls: 'x-fa fa-redo-alt',
             handler: 'hBtnRefresh'
@@ -14,14 +42,27 @@ Ext.define('PortalApp.view.dashboard.DashboardView', {
             hidden: '{fullscreen}'
         }
     },
-    items: [{
-        xtype: 'dashboard-subboardview',
-        height: '100%',
-        resizable: {handles: 's'}
-    }],
+    items: [],
     controller: 'dashboard-dashboardviewcontroller',
     viewModel: {
         data: {
+            dashboardTemplateId: null
+        },
+        stores: {
+            dashboardTemplateTree: {
+                fields: ['id', 'version', 'code', 'description', 'enabled', 'text', 'isRecord'],
+                type: 'entitytree',
+                transform: 'pathTree',
+                pathField: 'code',
+                application: 'portal',
+                domainType: 'org.psr.platform.portal.entity.DashboardTemplateEntity',
+                rootText: '概览模板',
+                autoLoad: true,
+                listeners: {
+                    load: 'onDataLoad'
+                }
+            }
         }
     }
-});
+})
+;
--- app/desktop/src/view/dashboard/DashboardViewController.js
+++ app/desktop/src/view/dashboard/DashboardViewController.js
@@ -5,11 +5,44 @@ Ext.define('PortalApp.view.dashboard.DashboardViewController', {
         this.loadData();
     },
     loadData: function () {
+        const view = this.getView();
         if (this.getView().rendered) {
-
+            const dashboardTemplateTreeStore = this.getStore('dashboardTemplateTree');
+            dashboardTemplateTreeStore.removeAll();
+            dashboardTemplateTreeStore.load();
+            view.remove(view.down('dashboard-subboardview'));
         }
     },
     hBtnRefresh: function () {
         this.loadData();
+    },
+    onTreePickSelect: function (picker, record) {
+        this.getViewModel().set('dashboardTemplateId', record.get('id'));
+        this.loadData();
+    },
+    onDataLoad: function () {
+        const view = this.getView(),
+            dashboardTemplateId = this.getViewModel().get('dashboardTemplateId'),
+            dashboardTemplate = this.getStore('dashboardTemplateTree').byIdMap[dashboardTemplateId];
+        if (dashboardTemplate) {
+            const dashboardConfigValue = dashboardTemplate.get('config');
+            this.initDashboard(dashboardConfigValue ? JSON.parse(dashboardConfigValue) : null);
+        }
+    },
+    initDashboard: function (config) {
+        const view = this.getView();
+        view.remove(view.down('dashboard-subboardview'));
+        if (config) {
+            const dashboardConfig = {
+                xtype: 'dashboard-subboardview',
+                height: '100%'
+            };
+            if (config.height) {
+                dashboardConfig.height = config.height;
+            }
+            dashboardConfig.boardConfig = config.boardConfig;
+            dashboardConfig.subBoardConfigs = config.subBoardConfigs;
+            view.add(dashboardConfig);
+        }
     }
 });
