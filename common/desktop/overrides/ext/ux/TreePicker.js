9effa492e7c0999afdbec1d0799ed9e42247ae01
zhoudd <328704089@qq.com>
2022-01-18 09:39:06 +0800

    bug fix TreePicker没有正确触发change事件

--- common/desktop/overrides/ext/ux/TreePicker.js
+++ common/desktop/overrides/ext/ux/TreePicker.js
@@ -56,4 +56,35 @@ Ext.define('PSR.overrides.Ext.ux.TreePicker', {
 
         return picker;
     },
+    setValue: function (value) {
+        var me = this,
+            record;
+        const originValue = me.value;
+        me.value = value;
+
+        if (me.store.loading) {
+            // Called while the Store is loading. Ensure it is processed by the onLoad method.
+            if (value != originValue) {
+                me.fireEvent('change', me, value, originValue);
+            }
+            return me;
+        }
+
+        // try to find a record in the store that matches the value
+        record = value ? me.store.getNodeById(value) : me.store.getRoot();
+
+        if (value === undefined) {
+            record = me.store.getRoot();
+            me.value = record.getId();
+        } else {
+            record = me.store.getNodeById(value);
+        }
+
+        // set the raw value to the record's display field if a record was found
+        me.setRawValue(record ? record.get(me.displayField) : '');
+        if (value != originValue) {
+            me.fireEvent('change', me, value, originValue);
+        }
+        return me;
+    },
 });
