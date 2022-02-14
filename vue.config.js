d2ee48ee65626e11634670f838f534c3f0b8a55a
zhoudd <328704089@qq.com>
2022-02-14 08:06:58 +0800

    项目发布路径改为相对路径

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const AutoImport = require('unplugin-auto-import/webpack')
 const Components = require('unplugin-vue-components/webpack')
 const {ElementPlusResolver} = require("unplugin-vue-components/resolvers")
 module.exports = {
+    publicPath: './',
     lintOnSave: false,
     configureWebpack: {
         plugins: [
