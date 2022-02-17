4eee369d7f86f89be622a829d33f625a5dd6b4dd
zhoudd <328704089@qq.com>
2022-02-17 10:27:10 +0800

    fix bug element-plus按需引入时messagebox样式失效，改为完整引入

--- src/main.js
+++ src/main.js
@@ -6,4 +6,10 @@ import {router} from "@/router";
 const app = createApp(App)
 app.use(store)
 app.use(router)
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+app.use(ElementPlus)
+
 app.mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,7 @@
-const AutoImport = require('unplugin-auto-import/webpack')
-const Components = require('unplugin-vue-components/webpack')
-const {ElementPlusResolver} = require("unplugin-vue-components/resolvers")
 module.exports = {
     publicPath: './',
     lintOnSave: false,
     configureWebpack: {
-        plugins: [
-            AutoImport({
-                resolvers: [ElementPlusResolver()]
-            }),
-            Components({
-                resolvers: [ElementPlusResolver()]
-            })
-        ]
+        plugins: []
     }
 }
