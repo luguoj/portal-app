4fa53865ff90e9716a525df6f3325d08eff5f98b
zhoudd <328704089@qq.com>
2022-02-17 10:27:43 +0800

    引入element-plus中文语言包

--- src/main.js
+++ src/main.js
@@ -8,8 +8,9 @@ app.use(store)
 app.use(router)
 
 import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import 'element-plus/dist/index.css'
 
-app.use(ElementPlus)
+app.use(ElementPlus, {locale: zhCn})
 
 app.mount('#app')
