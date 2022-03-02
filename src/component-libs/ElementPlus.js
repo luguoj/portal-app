901b5e6460cadc3ee47573dfd697e59c025b69de
zhoudd <328704089@qq.com>
2022-03-02 10:34:32 +0800

    引入element-plus消息样式

--- src/component-libs/ElementPlus.js
+++ src/component-libs/ElementPlus.js
@@ -1,4 +1,5 @@
 import 'element-plus/es/components/message-box/style'
+import 'element-plus/es/components/message/style'
 import {ElIcon} from "element-plus";
 
 export function useElementPlus(app) {
