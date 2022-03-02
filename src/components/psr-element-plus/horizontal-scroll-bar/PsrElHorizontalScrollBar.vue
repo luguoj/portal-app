a666d155221e9bb6b53cb046cdcf55fcbb27df61
zhoudd <328704089@qq.com>
2022-03-02 10:52:41 +0800

    PsrElHorizontalScrollBar 水平滚动组件

---
+++ src/components/psr-element-plus/horizontal-scroll-bar/PsrElHorizontalScrollBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-scrollbar class="ct-scrollbar" ref="refScrollbar" @wheel.prevent="handleScroll">
+    <div class="ct-scrollbar-inner">
+      <slot></slot>
+    </div>
+  </el-scrollbar>
+</template>
+
+<script>
+import {ref} from "vue";
+
+export default {
+  name: "PsrElHorizontalScrollBar",
+  setup() {
+    const refScrollbar = ref(null)
+
+    function handleScroll(e) {
+      const wheelDelta = e.wheelDelta || -e.deltaY * 40
+      const scrollbar = refScrollbar.value
+      scrollbar.setScrollLeft(scrollbar.wrap$.scrollLeft - wheelDelta)
+    }
+
+    return {refScrollbar, handleScroll}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.ct-scrollbar {
+
+  .ct-scrollbar-inner {
+    display: flex;
+    width: fit-content;
+    padding: 0 10px;
+  }
+}
+</style>
