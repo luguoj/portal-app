00fbe4da5257b000e3ed6873322f7ac485ac23bd
zhoudd <328704089@qq.com>
2022-03-14 08:18:59 +0800

    统一TreeTable与DataTable抬头插槽容器

--- src/component-libs/PrimeVue.js
+++ src/component-libs/PrimeVue.js
@@ -2,6 +2,7 @@ import PrimeVue from "primevue/config";
 import 'primevue/resources/primevue.min.css'
 import 'primevue/resources/themes/lara-light-blue/theme.css'
 import 'primeicons/primeicons.css'
+import './prime-vue/custom.scss'
 
 export function usePrimeVue(app) {
     app.use(PrimeVue)
