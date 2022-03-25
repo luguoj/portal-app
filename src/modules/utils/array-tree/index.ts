79ce6d474d1441951fc3e11e991a6400b4ce3ace
zhoudd <328704089@qq.com>
2022-03-25 08:33:42 +0800

    array-tree工具类 过滤方法

---
+++ src/modules/utils/array-tree/index.ts
@@ -0,0 +1,51 @@
+function getChildren<T extends Record<string | number | symbol, any>>(
+    node: T,
+    childrenProperty: keyof T
+): T[] | null {
+    if (node[childrenProperty] && Array.isArray(node[childrenProperty]) && node[childrenProperty].length > 0) {
+        return node[childrenProperty]
+    }
+    return null
+}
+
+export function filterFromBottom<T extends Record<string | number | symbol, any>>(
+    nodes: T[],
+    filterFn: (node: T) => boolean,
+    childrenProperty: keyof T = "children"
+): T[] {
+    const result: T[] = []
+    for (let i = 0; i < nodes.length; i++) {
+        const node = {...nodes[i]};
+        const flag = filterFn(node)
+        let filteredChildren: T[] | null = null
+        const children = getChildren(node, childrenProperty)
+        if (children) {
+            filteredChildren = filterFromBottom(children, filterFn, childrenProperty)
+            node[childrenProperty] = filteredChildren as T[keyof T]
+        }
+        if (flag || filteredChildren) {
+            result.push(node)
+        }
+    }
+    return result
+}
+
+export function filterFromRoot<T extends Record<string | number | symbol, any>>(
+    nodes: T[],
+    filterFn: (node: T) => boolean,
+    childrenProperty: keyof T = "children"
+): T[] {
+    const result: T[] = []
+    for (let i = 0; i < nodes.length; i++) {
+        const node = {...nodes[i]};
+        if (filterFn(node)) {
+            result.push(node)
+            const children = node[childrenProperty]
+            if (children) {
+                const filteredChildren = filterFromBottom(children, filterFn, childrenProperty) as unknown
+                node[childrenProperty] = filteredChildren as T[keyof T]
+            }
+        }
+    }
+    return result
+}
