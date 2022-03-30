546314c617555d05c07a55b013d3a14c7f775633
zhoudd <328704089@qq.com>
2022-03-30 15:58:12 +0800

    插件成员缓存应用上下文

--- src/libs/commons/app-context/AppPlugin.ts
+++ src/libs/commons/app-context/AppPlugin.ts
@@ -3,12 +3,14 @@ import {AppContext} from "./AppContext";
 
 export class AppPlugin {
     injectKey: string
+    appContext?: AppContext
 
     constructor(injectKey: string) {
         this.injectKey = injectKey
     }
 
     install(app: App, appContext: AppContext) {
+        this.appContext = appContext
         app.provide(this.injectKey, this)
     }
 }
