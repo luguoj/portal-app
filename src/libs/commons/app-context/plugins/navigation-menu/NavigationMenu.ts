4af4503e051cb5ab4ef06490984e46798df763e9
zhoudd <328704089@qq.com>
2022-03-30 11:35:54 +0800

    重构

--- src/libs/commons/app-context/plugins/navigation-menu/NavigationMenu.ts
+++ src/libs/commons/app-context/plugins/navigation-menu/NavigationMenu.ts
@@ -1,6 +1,5 @@
-import {NavigationMenuItem} from "./NavigationMenuItem";
+import {MenuInvalid, NavigationMenuItem} from "./NavigationMenuItem";
 import {NavigationMenuItemRaw} from "./NavigationMenuItemRaw";
-import {buildMenuItems} from "./buildMenuItems";
 import {ref} from "vue";
 import {AppPlugin} from "../../AppPlugin";
 
@@ -10,4 +9,27 @@ export class NavigationMenu extends AppPlugin {
     update(navigaionMenuRaw: NavigationMenuItemRaw[]) {
         this.menuItems.value = buildMenuItems(navigaionMenuRaw)
     }
+}
+
+function buildMenuItems(menuItems: NavigationMenuItemRaw[]): NavigationMenuItem[] {
+    return menuItems.map(item => {
+        if (item.children != undefined) {
+            if (item.children.length > 0) {
+                const newChildren = buildMenuItems(item.children)
+                if (newChildren.length > 0) {
+                    const {id, title, iconCls} = item
+                    return {id, title, iconCls, children: newChildren}
+                }
+            }
+        } else {
+            const {id, title, iconCls, route} = item
+            return {
+                id,
+                title: title || route.meta?.tag?.title || id,
+                iconCls: iconCls || route.meta?.tag?.iconCls || "pi pi-book",
+                route
+            }
+        }
+        return MenuInvalid
+    }).filter(item => item !== MenuInvalid)
 }
