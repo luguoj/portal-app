2b456489ecf742c4c982808b5fec3288945ddc25
zhoudd <328704089@qq.com>
2022-04-12 15:38:31 +0800

    fix bug 导航故障时也触发了当前路由变更事件

--- src/libs/commons/app-context/route/PsrAppRouter.ts
+++ src/libs/commons/app-context/route/PsrAppRouter.ts
@@ -1,4 +1,4 @@
-import {createRouter, createWebHashHistory, Router} from "vue-router";
+import {createRouter, createWebHashHistory, isNavigationFailure, Router} from "vue-router";
 import {PsrAppRouteRecord} from "./types/PsrAppRouteRecord";
 import {ref, Ref} from "vue";
 import {PsrAppCurrentRoute} from "@/libs/commons/app-context/route/types/PsrAppCurrentRoute";
@@ -21,41 +21,43 @@ export class PsrAppRouter {
             history: createWebHashHistory(),
             routes: routeRecords
         })
-        this.router.beforeEach(to => {
+        this.router.beforeEach(() => {
             NProgress.start()
         })
-        this.router.afterEach(to => {
-            // 路由跳转切换布局信息
-            const route = to
-            let layoutChange = false
-            let moduleChange = false
-            const newRoute: PsrAppCurrentRoute = {
-                layout: null,
-                module: null,
-                route
-            }
-            if (route.matched.length > 0 && route.name !== 'root') {
-                newRoute.layout = route.matched[0] as unknown as PsrAppRouteRecord
-                if (route.matched[0].name !== this.current.value.layout?.name) {
-                    console.log('切换应用布局')
-                    layoutChange = true
+        this.router.afterEach((to, from, failure) => {
+            if (!isNavigationFailure(failure)) {
+                // 成功导航，切换布局信息
+                const route = to
+                let layoutChange = false
+                let moduleChange = false
+                const newRoute: PsrAppCurrentRoute = {
+                    layout: null,
+                    module: null,
+                    route
                 }
-                if (route.matched.length > 1) {
-                    newRoute.module = route.matched[1] as unknown as PsrAppRouteRecord
-                    if (this.current.value.module?.name !== route.matched[1].name) {
-                        console.log('切换应用模块')
-                        moduleChange = true
+                if (route.matched.length > 0 && route.name !== 'root') {
+                    newRoute.layout = route.matched[0] as unknown as PsrAppRouteRecord
+                    if (route.matched[0].name !== this.current.value.layout?.name) {
+                        console.log('切换应用布局')
+                        layoutChange = true
+                    }
+                    if (route.matched.length > 1) {
+                        newRoute.module = route.matched[1] as unknown as PsrAppRouteRecord
+                        if (this.current.value.module?.name !== route.matched[1].name) {
+                            console.log('切换应用模块')
+                            moduleChange = true
+                        }
                     }
                 }
+                this.current.value = newRoute
+                if (layoutChange) {
+                    this._layoutChangeEvent.trigger(newRoute)
+                }
+                if (moduleChange) {
+                    this._moduleChangeEvent.trigger(newRoute)
+                }
+                this._routeChangeEvent.trigger(newRoute)
             }
-            this.current.value = newRoute
-            if (layoutChange) {
-                this._layoutChangeEvent.trigger(newRoute)
-            }
-            if (moduleChange) {
-                this._moduleChangeEvent.trigger(newRoute)
-            }
-            this._routeChangeEvent.trigger(newRoute)
             NProgress.done()
         })
     }
