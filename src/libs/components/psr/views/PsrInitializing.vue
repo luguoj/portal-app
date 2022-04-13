1b556342f07f4c17d58819ef895b70d1ed131444
zhoudd <328704089@qq.com>
2022-04-13 13:21:27 +0800

    初始化页面

---
+++ src/libs/components/psr/views/PsrInitializing.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Initializing...</h1>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "psr-initializing"
+})
+</script>
+
+<style scoped>
+
+</style>
