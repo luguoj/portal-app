2c1977036bc530f1a73d05b20b513e78d75af836
zhoudd <328704089@qq.com>
2022-04-19 11:35:08 +0800

    重构插件接口

--- src/libs/commons/app-context/types/PsrAppPlugin.ts
+++ src/libs/commons/app-context/types/PsrAppPlugin.ts
@@ -3,16 +3,22 @@ import {PsrAppContext} from "../PsrAppContext";
 
 export abstract class PsrAppPlugin {
     injectKey: string
-    appContext?: PsrAppContext
+    private _appContext?: PsrAppContext
 
     constructor(injectKey: string) {
         this.injectKey = injectKey
     }
 
     install(app: App, appContext: PsrAppContext) {
-        this.appContext = appContext
+        this._appContext = appContext
         app.provide(this.injectKey, this)
     }
 
-    abstract onLayoutChange(): void
+    appContext() {
+        if (this._appContext) {
+            return this._appContext
+        } else {
+            throw new Error("应用插件未安装到上下文上")
+        }
+    }
 }
