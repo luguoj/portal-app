15594a72524123676a7a4be61984657eb8067620
zhoudd <328704089@qq.com>
2022-04-19 14:50:22 +0800

    开启保存时校验语法

--- vue.config.js
+++ vue.config.js
@@ -2,16 +2,11 @@ const {defineConfig} = require('@vue/cli-service')
 const AutoImport = require('unplugin-auto-import/webpack')
 const Components = require('unplugin-vue-components/webpack')
 const {ElementPlusResolver} = require("unplugin-vue-components/resolvers")
-const path = require("path");
-
-function resolvePath(dir) {
-    return path.join(__dirname, dir)
-}
 
 module.exports = defineConfig({
     transpileDependencies: true,
     publicPath: './',
-    lintOnSave: false,
+    lintOnSave: true,
     configureWebpack: {
         plugins: [
             AutoImport({
