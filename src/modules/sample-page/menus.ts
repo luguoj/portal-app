7222afaded0eef38b6597ad59ec732887800f271
zhoudd <328704089@qq.com>
2022-04-22 16:53:20 +0800

    样例菜单增加层级

--- src/modules/sample-page/menus.ts
+++ src/modules/sample-page/menus.ts
@@ -1,14 +1,19 @@
 import {ROUTE_SAMPLE, ROUTE_SAMPLE_2, ROUTE_SAMPLE_PARENT} from "@/modules/sample-page/route";
 
 export const menus = [{
+    route: ROUTE_SAMPLE
+}, {
     id: 'sample-page',
     title: '样例页面',
     iconCls: 'pi pi-book',
     children: [{
-        route: ROUTE_SAMPLE
-    }, {
         route: ROUTE_SAMPLE_PARENT
     }, {
-        route: ROUTE_SAMPLE_2
+        id: 'sample-page-level-2',
+        title: '二级菜单',
+        iconCls: 'pi pi-book',
+        children: [{
+            route: ROUTE_SAMPLE_2
+        }]
     }]
 }]
