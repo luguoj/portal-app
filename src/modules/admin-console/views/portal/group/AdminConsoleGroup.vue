da020dd3cc24c90d49719579742a64e68f89a679
zhoudd <328704089@qq.com>
2022-04-24 16:40:16 +0800

    缓存子路由所有组件

--- src/modules/admin-console/views/portal/group/AdminConsoleGroup.vue
+++ src/modules/admin-console/views/portal/group/AdminConsoleGroup.vue
@@ -1,6 +1,6 @@
 <template>
   <router-view v-slot="{Component}">
-    <keep-alive include="admin-console-group-list,admin-console-group-permission">
+    <keep-alive>
       <component :is="Component"/>
     </keep-alive>
   </router-view>
