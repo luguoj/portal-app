ba9b952bdcd3de9c2e660e4c9a8ea86ab1ab321e
zhoudd <328704089@qq.com>
2022-04-25 09:15:23 +0800

    fix bug 布局路由离开时清除布局store代理的布局名称标识，防相同布局组件复用时产生异常

--- src/libs/commons/app-context/LayoutStoreProxyProvider.ts
+++ src/libs/commons/app-context/LayoutStoreProxyProvider.ts
@@ -1,4 +1,4 @@
-import {RouteLocationNormalized} from "vue-router";
+import {onBeforeRouteLeave, RouteLocationNormalized} from "vue-router";
 import {computed, inject, provide, ref} from "vue";
 import {useAppContext} from "./PsrAppContextProvider";
 import {ComputedRef} from "@vue/reactivity";
@@ -12,26 +12,30 @@ interface LayoutStoreProxy<S> {
     dispatch: (action: string, payload?: any) => Promise<any>
 }
 
-export function updateLayoutStoreModuleName(to: RouteLocationNormalized, vm: any) {
+export function onLayoutRouteEnter(to: RouteLocationNormalized, vm: any) {
     if (to.matched.length > 0) {
-        vm.layoutStoreModuleName = to.matched[0].name
+        vm.layoutName = to.matched[0].name
     } else {
-        vm.layoutStoreModuleName = null
+        vm.layoutName = null
     }
 }
 
 export function createLayoutStoreProxy<S>() {
     const {store: {store}} = useAppContext()
-    const layoutStoreModuleName = ref<string | null>(null)
+    const layoutName = ref<string | null>(null)
+    onBeforeRouteLeave(() => {
+        console.log('LayoutStoreProxy.beForeRouteLeave=>清除layoutName')
+        layoutName.value = null
+    })
     const layoutStore = computed<LayoutStoreProxy<S> | null>(() => {
-        if (layoutStoreModuleName.value) {
+        if (layoutName.value) {
             return {
-                state: store.state[layoutStoreModuleName.value] as S,
+                state: store.state[layoutName.value] as S,
                 commit: function (mutation: string, payload?: any) {
-                    store.commit(`${layoutStoreModuleName.value}/${mutation}`, payload)
+                    store.commit(`${layoutName.value}/${mutation}`, payload)
                 },
                 dispatch: function (action: string, payload?: any) {
-                    return store.dispatch(`${layoutStoreModuleName.value}/${action}`, payload)
+                    return store.dispatch(`${layoutName.value}/${action}`, payload)
                 }
             }
         } else {
@@ -41,7 +45,7 @@ export function createLayoutStoreProxy<S>() {
     provide(KEY, layoutStore)
     return {
         layoutStore,
-        layoutStoreModuleName
+        layoutName
     }
 }
 
