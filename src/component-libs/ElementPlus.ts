97ce575866874942b9f7a759efee264a48134aff
zhoudd <328704089@qq.com>
2022-04-26 11:23:15 +0800

    .el-header.fit适应内容的抬头样式 PsrElToolBar工具栏组件

--- src/component-libs/ElementPlus.ts
+++ src/component-libs/ElementPlus.ts
@@ -1,9 +1,7 @@
 import 'element-plus/es/components/message-box/style/index'
 import 'element-plus/es/components/message/style/index'
-import {ElIcon} from "element-plus";
 import {App} from "@vue/runtime-core";
 import './element-plus/custom.scss'
 
 export function useElementPlus(app: App): void {
-    app.component('ElIcon', ElIcon)
 }
---
+++ src/libs/components/psr/element-plus/PsrElToolbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="ct-root">
+    <el-space wrap>
+      <slot/>
+    </el-space>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "PsrElToolbar"
+})
+</script>
+
+<style lang="scss" scoped>
+.ct-root {
+  height: auto;
+  padding: 0.5rem;
+  background-color: var(--el-border-color-extra-light);
+  border-bottom: var(--psr-border);
+}
+</style>
