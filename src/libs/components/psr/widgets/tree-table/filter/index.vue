06f6a499d4a3045e652adbe2ed2248c4cd09c574
zhoudd <328704089@qq.com>
2022-04-29 08:36:16 +0800

    PsrFilterTreeTable过滤树表格组件

---
+++ src/libs/components/psr/widgets/tree-table/filter/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <p-tree-table
+      class="p-treetable-sm"
+      :scrollable="true"
+      scrollHeight="flex"
+      :filters="model.filters"
+      filterMode="lenient"
+      :value="model.rootNodes"
+  >
+    <slot/>
+  </p-tree-table>
+</template>
+
+<script lang="ts">
+import {defineComponent, PropType} from "vue";
+import PTreeTable from "primevue/treetable";
+import {PsrFilterTreeTableModel} from "@/libs/components/psr/widgets/tree-table/filter/PsrFilterTreeTableModel";
+
+export default defineComponent({
+  name: "psr-filter-tree-table",
+  components: {
+    PTreeTable
+  },
+  props: {
+    model: {
+      type: Object as PropType<PsrFilterTreeTableModel<any>>,
+      required: true
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
