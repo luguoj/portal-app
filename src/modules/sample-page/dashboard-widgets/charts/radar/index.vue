da709cfac9079cdfaa304bdb0766b2e8768e8c1f
zhoudd <328704089@qq.com>
2022-05-06 14:14:57 +0800

    概览组件样例

---
+++ src/modules/sample-page/dashboard-widgets/charts/radar/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    {{ title }}123123sadfsdf
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "sample-dashboard-widget-chart-radar",
+  components: {},
+  setup() {
+    return {
+      title: 'hello'
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
