2ad6b5c147a63be9c8112a95e648dcbc15a4330f
zhoudd <328704089@qq.com>
2022-05-07 16:41:53 +0800

    适配类型修改，增加折线图样例

--- src/plugin-config/psr-app-context/dashboardWidgetManager.ts
+++ src/plugin-config/psr-app-context/dashboardWidgetManager.ts
@@ -1,12 +1,17 @@
-import {createDashboardWidgetManager} from "@/modules/dashboard/plugins/PsrDashboardWidgetManagerProvider";
+import {createDashboardWidgetManager} from "@/modules/dashboard/plugins/widget-manager/PsrDashboardWidgetManagerProvider";
 import {defineAsyncComponent} from "vue";
 
 const dashboardWidgetManage = createDashboardWidgetManager({
     widgets: [{
         name: 'sample/charts',
-        title: 'sample/charts',
+        title: '样例/图表',
         widgets: [{
+            name: 'line',
+            title: '折线图',
+            component: defineAsyncComponent(() => import("@/modules/sample-page/dashboard-widgets/charts/line/index.vue"))
+        }, {
             name: 'radar',
+            title: '雷达图',
             component: defineAsyncComponent(() => import("@/modules/sample-page/dashboard-widgets/charts/radar/index.vue"))
         }]
     }]
