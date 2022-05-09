77498138f0f78a63e14a46007462f78e3e7952af
zhoudd <328704089@qq.com>
2022-05-09 15:09:04 +0800

    breakpoint选项信息

--- src/modules/dashboard/views/masonry/components/header-bar.vue
+++ src/modules/dashboard/views/masonry/components/header-bar.vue
@@ -14,7 +14,6 @@
 <script lang="ts">
 import {defineComponent} from "vue";
 import PsrToolbar from "@/libs/components/psr/widgets/toolbar/base/index.vue";
-import {breakpointSelections} from "@/modules/dashboard/types/TemplateConfig";
 
 export default defineComponent({
   name: "header-bar",
@@ -34,7 +33,22 @@ export default defineComponent({
   emits: ['update:editing', 'update:breakpoint'],
   setup() {
     return {
-      breakpointSelections
+      breakpointSelections: [{
+        title: '大屏 > 1200px',
+        breakpoint: 'lg',
+      }, {
+        title: '中屏 992px ~ 1200px',
+        breakpoint: 'md',
+      }, {
+        title: '小屏 678px ~ 992px',
+        breakpoint: 'sm',
+      }, {
+        title: '超小屏 480px ~ 678px',
+        breakpoint: 'xs',
+      }, {
+        title: '特小屏 0 ~ 480px',
+        breakpoint: 'xxs',
+      }]
     }
   }
 })
