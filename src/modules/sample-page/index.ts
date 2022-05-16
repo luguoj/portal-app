4cc83f3b692f1dbe8fee2d58105dd4f1e700640a
zhoudd <328704089@qq.com>
2022-05-16 16:17:23 +0800

    样例模块widgets增加目录图标和项目缩略图样式

--- src/modules/sample-page/index.ts
+++ src/modules/sample-page/index.ts
@@ -3,6 +3,7 @@ import {routes} from "./route";
 import {menus} from "./menus";
 import {PsrAppWidgetCatalogRaw} from "@/libs/commons/psr/app-context/widget-manager";
 import {defineAsyncComponent, markRaw} from "vue";
+import './asserts/style/widgets.scss'
 
 export const SamplePage: PsrAppModuleOptions = {
     name: "sample-page",
@@ -13,15 +14,18 @@ export const SamplePage: PsrAppModuleOptions = {
 export const SamplePageWidgets: PsrAppWidgetCatalogRaw[] = [{
     name: 'sample/charts',
     title: '样例/图表',
+    iconCls: 'psr-sample-page-widget-chart',
     widgets: [{
         name: 'line',
         title: '折线图',
         component: markRaw(defineAsyncComponent(() => import("@/modules/sample-page/widgets/charts/line/index.vue"))),
+        thumbnailCls: 'psr-sample-page-widget-chart-line',
         permissions: []
     }, {
         name: 'radar',
         title: '雷达图',
         component: markRaw(defineAsyncComponent(() => import("@/modules/sample-page/widgets/charts/radar/index.vue"))),
+        thumbnailCls: 'psr-sample-page-widget-chart-radar',
         permissions: []
     }]
 }]
