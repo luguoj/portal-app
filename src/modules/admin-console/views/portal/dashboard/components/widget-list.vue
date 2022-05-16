cecba336ed46f7a259747f73cd41baab3af3b59f
zhoudd <328704089@qq.com>
2022-05-16 16:15:22 +0800

    widget-list增加目录图标显示，项目缩略图显示

--- src/modules/admin-console/views/portal/dashboard/components/widget-list.vue
+++ src/modules/admin-console/views/portal/dashboard/components/widget-list.vue
@@ -1,20 +1,19 @@
 <template>
   <el-collapse>
-    <el-collapse-item v-for="catalog in widgets" :key="catalog.name" :title="catalog.title">
-      <div
-          v-for="widget in catalog.widgets" :key="widget.name"
-          style="padding:10px;height:90px;width:160px"
-      >
-        <div class="widget-item" style="height:100%;width:100%;" @click="$emit('addWidget',widget)">
-          <el-skeleton style="height:100%;width:100%;">
-            <template #template>
-              <div style="position: absolute">{{ widget.title }}</div>
-              <el-skeleton-item variant="image" style="height:100%;width:100%;"/>
-            </template>
-          </el-skeleton>
-        </div>
-      </div>
-    </el-collapse-item>
+    <el-menu>
+      <el-sub-menu v-for="catalog in widgets" :key="catalog.name" :index="catalog.name">
+        <template #title>
+          <el-icon class="psr-widget-catalog-icon" :class="catalog.iconCls"/>
+          <span>{{ catalog.title }}</span>
+        </template>
+        <el-menu-item v-for="widget in catalog.widgets" :key="widget.name" :index="widget.name" class="widget-item">
+          <div class="widget-item-inner" @click="$emit('addWidget',widget)">
+            <div class="psr-widget-thumbnail" :class="widget.thumbnailCls?widget.thumbnailCls:''"/>
+            <div class="title">{{ widget.title }}</div>
+          </div>
+        </el-menu-item>
+      </el-sub-menu>
+    </el-menu>
   </el-collapse>
 </template>
 
@@ -35,9 +34,39 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
+.psr-widget-catalog-icon {
+  background-size: 18px;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
 .widget-item {
-  &:hover {
-    cursor: pointer;
+  --thumbnail-height: 100px;
+  --thumbnail-width: 190px;
+  height: var(--thumbnail-height);
+  padding: 0 10px !important;
+
+  .widget-item-inner {
+    height: var(--thumbnail-height);
+    width: var(--thumbnail-width);
+
+    .psr-widget-thumbnail {
+      height: var(--thumbnail-height);
+      width: var(--thumbnail-width);
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: contain;
+      opacity: 0.7;
+    }
+
+    .title {
+      position: absolute;
+      height: var(--thumbnail-height);
+      width: var(--thumbnail-width);
+      line-height: var(--thumbnail-height);
+      top: 0;
+      text-align: center;
+    }
   }
 }
 </style>
