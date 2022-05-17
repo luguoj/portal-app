2997147d59dbd4111db4200668fa38ec4dcc96a3
zhoudd <328704089@qq.com>
2022-05-17 08:55:18 +0800

    响应式测试样例

--- src/modules/sample-page/views/SamplePage.vue
+++ src/modules/sample-page/views/SamplePage.vue
@@ -1,24 +1,56 @@
 <template>
-  <section>
-    <h1>This is sample-page</h1>
-    <el-icon class="icon pi pi-book"></el-icon>
-    <el-button @click="click">click</el-button>
-    <el-input v-model="input"></el-input>
-    <br>
-  </section>
+  <el-container>
+    <el-aside>
+      <el-container>
+        <el-header>
+          <el-button @click="add">add</el-button>
+        </el-header>
+        <el-main>
+          <ul>
+            <li v-for="item in listModel" @click="editorModel=item">{{ item.name }}</li>
+          </ul>
+        </el-main>
+      </el-container>
+    </el-aside>
+    <el-main>
+      <el-form :model="editorModel">
+        <el-form-item label="name">
+          <input v-model="editorModel.name"/>
+<!--          <el-input v-model="editorModel.name"/>-->
+        </el-form-item>
+        <el-form-item label="age">
+          <el-input v-model="editorModel.age"/>
+        </el-form-item>
+        <el-form-item label="sex">
+          <el-input v-model="editorModel.sex"/>
+        </el-form-item>
+      </el-form>
+    </el-main>
+  </el-container>
 </template>
 
 <script lang="ts">
-import {defineComponent, ref} from "vue";
+import {defineComponent, reactive, ref} from "vue";
+
+interface Person {
+  name: string
+  sex: string
+  age: number
+}
 
 export default defineComponent({
   name: "SamplePage",
   components: {},
   setup() {
+    const editorModel = ref<Person>({name: '', sex: '', age: 0})
+    const listModel = reactive<Person[]>([])
     return {
-      input: ref(),
-      click: () => {
-        console.log('click')
+      editorModel,
+      listModel,
+      add: () => {
+        const person: Person = {name: 'new', sex: '', age: 0}
+        editorModel.value = person
+        listModel.push(person)
       }
     }
   }
