5dbd6d49554fbe0682727a9746e8fbcdd04244c1
zhoudd <328704089@qq.com>
2022-06-17 15:29:24 +0800

    feat: 全局注册element-plus/icons-vue

--- src/plugin-config/element-plus/index.ts
+++ src/plugin-config/element-plus/index.ts
@@ -2,6 +2,10 @@ import 'element-plus/es/components/message-box/style/index'
 import 'element-plus/es/components/message/style/index'
 import {App} from "@vue/runtime-core";
 import './custom.scss'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 export function applyElementPlus(app: App): void {
+    for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+        app.component(key, component)
+    }
 }
