87be038c60ff64965ce8e4937b38ba28f6e47070
zhoudd <328704089@qq.com>
2022-06-17 10:18:57 +0800

    refactor: 适配应用上下文导航菜单升级，定义侧边栏分级菜单

--- src/modules/admin-console/menus.ts
+++ src/modules/admin-console/menus.ts
@@ -1,27 +1,29 @@
 import {ROUTE_AUTHORIZATION_USER, ROUTE_PORTAL_DASHBOARD, ROUTE_PORTAL_GROUP, ROUTE_PORTAL_PERMISSION, ROUTE_PORTAL_USER} from "./route";
-import {PsrAppNavigationMenuItemRaw} from "@/libs/commons/psr/app-context/navigation-menu";
+import {PsrAppNavigationMenuItemsRaw} from "@/libs/commons/psr/app-context/navigation-menu";
 import {ROUTE_ENTITY} from "@/modules/admin-console/route/entity";
 
-export const menus: PsrAppNavigationMenuItemRaw[] = [{
-    id: 'admin-console-authorization',
-    title: '授权管理',
-    iconCls: 'pi pi-key',
-    children: [{
-        route: ROUTE_AUTHORIZATION_USER
+export const menus: PsrAppNavigationMenuItemsRaw = {
+    aside: [{
+        id: 'admin-console-authorization',
+        title: '授权管理',
+        iconCls: 'pi pi-key',
+        children: [{
+            route: ROUTE_AUTHORIZATION_USER
+        }]
+    }, {
+        id: 'admin-console-portal',
+        title: '门户管理',
+        iconCls: 'pi pi-cog',
+        children: [{
+            route: ROUTE_PORTAL_USER
+        }, {
+            route: ROUTE_PORTAL_GROUP
+        }, {
+            route: ROUTE_PORTAL_PERMISSION
+        }, {
+            route: ROUTE_PORTAL_DASHBOARD
+        }]
+    }, {
+        route: ROUTE_ENTITY
     }]
-}, {
-    id: 'admin-console-portal',
-    title: '门户管理',
-    iconCls: 'pi pi-cog',
-    children: [{
-        route: ROUTE_PORTAL_USER
-    }, {
-        route: ROUTE_PORTAL_GROUP
-    }, {
-        route: ROUTE_PORTAL_PERMISSION
-    }, {
-        route: ROUTE_PORTAL_DASHBOARD
-    }]
-}, {
-    route: ROUTE_ENTITY
-}]
+}
