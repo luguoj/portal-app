f1884e38ac85528cb878429bbaf8f8b58b83f54b
zhoudd <328704089@qq.com>
2022-09-13 16:49:04 +0800

    feat: 黑暗主题自动切换prime-vue组件配色

--- src/config/prime-vue/index.ts
+++ src/config/prime-vue/index.ts
@@ -1,6 +1,5 @@
 import PrimeVue from "primevue/config";
 import 'primevue/resources/primevue.min.css'
-import 'primevue/resources/themes/lara-light-blue/theme.css'
 import 'primeicons/primeicons.css'
 import './custom.scss'
 import {App} from "@vue/runtime-core";
