9d05e2a3afd91fade2f37b26791835edc229feb7
zhoudd <328704089@qq.com>
2022-11-10 13:59:51 +0800

    refactor: 清理代码

--- src/modules/personal-center/index.ts
+++
@@ -1,9 +0,0 @@
-import {PsrAppModuleOptions} from "@/libs/commons/psr/app-context/";
-import {routes} from "./route";
-import {menus} from "./menus";
-
-export const PersonalCenter: PsrAppModuleOptions = {
-    name: "personal-center",
-    routes,
-    menus
-}
--- src/modules/personal-center/menus.ts
+++
@@ -1,8 +0,0 @@
-import {PsrAppNavigationMenuItemsRaw} from "@/libs/commons/psr/app-context/navigation-menu";
-import {ROUTE_PERSONAL_CENTER} from "@/modules/personal-center/route";
-
-export const menus: PsrAppNavigationMenuItemsRaw = {
-    userPopover: [{
-        route: ROUTE_PERSONAL_CENTER
-    }]
-}
