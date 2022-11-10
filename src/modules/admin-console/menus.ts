7675587b5d1e45591a250345e191c62069aac76d
zhoudd <328704089@qq.com>
2022-11-10 08:07:19 +0800

    refactor: 管理员控制台-用户管理功能重构到独立授权模块中

--- src/modules/admin-console/menus.ts
+++ src/modules/admin-console/menus.ts
@@ -1,15 +1,8 @@
-import {ROUTE_AUTHORIZATION_USER, ROUTE_ENTITY, ROUTE_ORGANIZATION, ROUTE_PORTAL_DASHBOARD, ROUTE_PORTAL_GROUP, ROUTE_PORTAL_PERMISSION, ROUTE_PORTAL_USER} from "./route";
+import {ROUTE_ENTITY, ROUTE_ORGANIZATION, ROUTE_PORTAL_DASHBOARD, ROUTE_PORTAL_GROUP, ROUTE_PORTAL_PERMISSION, ROUTE_PORTAL_USER} from "./route";
 import {PsrAppNavigationMenuItemsRaw} from "@/libs/commons/psr/app-context/navigation-menu";
 
 export const menus: PsrAppNavigationMenuItemsRaw = {
     aside: [{
-        id: 'admin-console-authorization',
-        title: '授权管理',
-        iconCls: 'pi pi-key',
-        children: [{
-            route: ROUTE_AUTHORIZATION_USER
-        }]
-    }, {
         id: 'admin-console-portal',
         title: '门户管理',
         iconCls: 'pi pi-cog',
--- src/modules/admin-console/route/index.ts
+++ src/modules/admin-console/route/index.ts
@@ -1,15 +1,12 @@
 import {PsrAppRouteRecordRaw} from "@/libs/commons/psr/app-context/route/types/PsrAppRouteRecordRaw";
 import {ROUTE_PORTAL_DASHBOARD, ROUTE_PORTAL_GROUP, ROUTE_PORTAL_PERMISSION, ROUTE_PORTAL_USER} from "./portal";
-import {ROUTE_AUTHORIZATION_USER} from "./authorization";
 import {ROUTE_ENTITY} from "@/modules/admin-console/route/entity";
 import {ROUTE_ORGANIZATION} from "@/modules/admin-console/route/organization";
 
 export * from './entity'
-export * from './authorization'
 export * from './portal'
 export * from './organization'
 export const routes: Array<PsrAppRouteRecordRaw> = [
-    ROUTE_AUTHORIZATION_USER,
     ROUTE_PORTAL_PERMISSION,
     ROUTE_PORTAL_GROUP,
     ROUTE_PORTAL_USER,
