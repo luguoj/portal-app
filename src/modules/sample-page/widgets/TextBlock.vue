acff54ebff9800ab6c6ce4a362cd473ce25f3fcf
zhoudd <328704089@qq.com>
2022-11-21 13:26:54 +0800

    refactor: 使用概览插件工具类获取数据

--- src/modules/sample-page/widgets/TextBlock.vue
+++ src/modules/sample-page/widgets/TextBlock.vue
@@ -1,14 +1,19 @@
 <template>
-  {{ screenItemData }}
+  <div>{{ data }}</div>
 </template>
 
 <script lang="ts">
 export default {
-  name: "TextBlock",
-  props: {
-    screenItemData: String
-  }
+  name: "TextBlock"
 }
+</script>
+<script setup lang="ts">
+import {DataProviderProperties, useData} from "@psr-framework/vue3-plugin-dashboard"
+
+const props = defineProps<{
+  itemProperties: DataProviderProperties
+}>()
+const data = useData(props.itemProperties)
 </script>
 
 <style scoped>
